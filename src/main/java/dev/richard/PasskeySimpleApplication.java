ca0d367cd37107a639174394365261c16eadb0dd
Ricardolv <richardluizv@gmail.com>
2025-05-29 14:12:58 -0300

    create project simple passkey

---
+++ src/main/java/dev/richard/PasskeySimpleApplication.java
@@ -0,0 +1,13 @@
+package dev.richard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasskeySimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasskeySimpleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/richard/application/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package dev.richard.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("richard")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        req->
+                                req.anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults())
+                .webAuthn(
+                        webAuthn -> webAuthn
+                                .rpName("Spring Security Relyinq Party")
+                                .rpId("localhost")
+                                .allowedOrigins("http://localhost:8080")
+                );
+
+        return http.build();
+    }
+
+}
